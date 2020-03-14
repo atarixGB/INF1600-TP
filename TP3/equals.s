790918187fc8e1d5648f2e3d02d63ef41b8db951
legil <lea-kim.gilbert@polymtl.ca>
2020-03-13 22:47:58 -0400

    equals.s completed

--- TP3/equals.s
+++ TP3/equals.s
@@ -10,15 +10,19 @@
     ----------------
 */
 
-.globl matrix_equals_asm
+.global matrix_equals_asm
 
 # --------------------------------------
 # ---------- WORK IN PROGRESS ----------
 # --------------------------------------
 
-matrix_equals_asecxm:
+matrix_equals_asm:
         push %ebp      /* Save old base pointer */
         mov %esp, %ebp /* Set ebp to current esp */
+
+        pushl %ebx              # pour pouvoir utiliser ebx, edi, esi
+        pushl %edi
+        pushl %esi
 
         subl $8, %esp           # pour allouer de l'espace sur la pile aux 
                                 # variables locales (c, r)
@@ -30,22 +34,39 @@ matrix_equals_asecxm:
 
 for_loop_rows:
         movl 16(%ebp), %eax     # eax = matorder
-        cmpl %eax, %ecx         # matorder == r ? 
+        cmpl %eax, %ecx         # matorder <= r ? 
         jge equal               # si r > matorder, sauter a "equal"
 
 for_loop_columns:  
-        cmpl %eax, %edx         # matorder == c ?
+        cmpl %eax, %edx         # matorder <= c ?
         jge change_row          # si c > matorder, sauter a "change_row
 
-/*
-if:
-        jmp not_equal           # branchement non conditionel vers "not_equal"
+        # condition if
+        # matdata1[i]
+        movl 8(%ebp), %ebx      # ebx = matdata1
+        movl -4(%ebp), %eax     # eax = r
+        mul %ebx                # eax = r * matdata1
+        movl %eax, %esi         # esi = r * matdata1
+        movl -8(%ebp), %eax     # eax = c
+        addl %esi, %esi         # esi = c (eax) +  r * matdata1 (esi)
+
+        # matdata2[i]
+        movl 12(%ebp), %ebx      # ebx = matdata2
+        movl -4(%ebp), %eax     # eax = r
+        mul %ebx                # eax = r * matdata2
+        movl %eax, %esi         # esi = r * matdata2
+        movl -8(%ebp), %eax     # eax = c
+        addl %esi, %edi         # edi = c (eax) +  r * matdata2 (esi)
+
+        cmp %esi, %edi          # matdata1[i] != matdata2[i] ?       
+        jne not_equal           # branchement non conditionel vers "not_equal"
+        movl %edx, %eax;        # eax = c
+        addl $1, %edx           # edx = c + 1
 
 change_row:
         movl %ecx, %eax         # eax = r
-        addl $1, %ecx           # ecx = eax + 1 = r + 1
-        jmp for_loop_columns    # branchement non conditionel vers "for_loop_columns"
-*/
+        addl $1, %ecx           # ecx = r + 1
+        jmp for_loop_rows       # branchement non conditionel vers "for_loop_rows"
 
 not_equal:
         movl $0, %eax           # eax = 0 (valeur de retour)
@@ -55,5 +76,9 @@ equal:
         movl $1, %eax           # eax = 1 (valeur de retour)
 
 end:
+        subl $8, %esp           # pour depiler les variables locales
+        popl %esi               # pour depiler les registres esi, edi et ebx      
+        popl %edi
+        popl %ebx
         leave          /* Restore ebp and esp */
-        ret            /* Rnot_equaleturn to the caller */
+        ret            /* Return to the caller */
