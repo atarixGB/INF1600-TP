47557d43a03a8aee76bbbac190e0a977dd4fccce
legil <lea-kim.gilbert@polymtl.ca>
2020-03-14 15:51:32 -0400

    change  equals.s

--- TP3/equals.s
+++ TP3/equals.s
@@ -24,48 +24,59 @@ matrix_equals_asm:
         pushl %edi
         pushl %esi
 
-        subl $8, %esp           # pour allouer de l'espace sur la pile aux 
-                                # variables locales (c, r)
+        subl $16, %esp          # pour allouer de l'espace sur la pile aux 
+                                # variables locales (c, r) et +
         
         movl $0, -4(%ebp)       # r = 0
-        movl -4(%ebp), %ecx     # ecx = r = 0
         movl $0, -8(%ebp)       # c = 0
-        movl -8(%ebp), %edx     # edx = c = 0
 
 for_loop_rows:
         movl 16(%ebp), %eax     # eax = matorder
-        cmpl %eax, %ecx         # matorder <= r ? 
+        movl -4(%ebp), %ecx     # ecx = r
+        cmp %eax, %ecx          # matorder <= r ? 
         jge equal               # si r > matorder, sauter a "equal"
 
 for_loop_columns:  
-        cmpl %eax, %edx         # matorder <= c ?
-        jge change_row          # si c > matorder, sauter a "change_row
+        movl -8(%ebp), %ecx     # ecx = c
+        cmp %eax, %ecx          # matorder <= c ?
+        jge increment_col       # si c > matorder, sauter a "increment_col"
 
         # condition if
-        # matdata1[i]
-        movl 8(%ebp), %ebx      # ebx = matdata1
+        # intmatdata1[c +  r * matorder]
+        movl 16(%ebp), %ebx     # ebx = matorder
         movl -4(%ebp), %eax     # eax = r
-        mul %ebx                # eax = r * matdata1
-        movl %eax, %esi         # esi = r * matdata1
+        mul %ebx                # eax = r * matorder
+        movl %eax, %esi         # esi = r * matorder
         movl -8(%ebp), %eax     # eax = c
-        addl %esi, %esi         # esi = c (eax) +  r * matdata1 (esi)
+        addl %esi, %eax         # eax = c + r * matorder
+        movl 8(%ebp), %edi      # edi = inmatdata1 (adresse)
+        movl (%edi,%eax,4), %eax  # eax = inmatdata1[c +  r * matorder]
+        movl %eax, -12(%ebp)    # -12(ebp) = inmatdata2[c +  r * matorder]
 
-        # matdata2[i]
-        movl 12(%ebp), %ebx      # ebx = matdata2
+        # intmatdata2[c +  r * matorder]
+        movl 16(%ebp), %ebx     # ebx = matorder
         movl -4(%ebp), %eax     # eax = r
-        mul %ebx                # eax = r * matdata2
-        movl %eax, %esi         # esi = r * matdata2
+        mul %ebx                # eax = r * matorder
+        movl %eax, %esi         # esi = r * matorder
         movl -8(%ebp), %eax     # eax = c
-        addl %esi, %edi         # edi = c (eax) +  r * matdata2 (esi)
+        addl %esi, %eax         # eax = c + r * matorder
+        movl 12(%ebp), %edi     # edi = inmatdata2 (adresse)
+        movl (%edi,%eax,4), %eax  # eax = inmatdata2[c +  r * matorder]
+        movl %eax, -16(%ebp)    # -12(ebp) = inmatdata2[c +  r * matorder]
 
-        cmp %esi, %edi          # matdata1[i] != matdata2[i] ?       
+        # comparaison des valeurs de chaque matrice
+        movl -12(%ebp), %esi    # esi = inmatdata1[c +  r * matorder]
+        movl -16(%ebp), %edi    # edi = inmatdata2[c +  r * matorder]
+        cmp %esi, %edi          # matdata1[c +  r * matorder] != matdata2[c +  r * matorder] ?       
         jne not_equal           # branchement non conditionel vers "not_equal"
-        movl %edx, %eax;        # eax = c
-        addl $1, %edx           # edx = c + 1
+        movl -8(%ebp), %eax     # eax = c
+        addl $1, -8(%ebp)       # c = c + 1
+        jmp for_loop_columns       # branchement vers for_loop_columns
 
-change_row:
-        movl %ecx, %eax         # eax = r
+increment_col:
+        movl -4(%ebp), %eax     # eax = r
         addl $1, %ecx           # ecx = r + 1
+        movl %ecx, -4(%ebp)     # r = r + 1
         jmp for_loop_rows       # branchement non conditionel vers "for_loop_rows"
 
 not_equal:
@@ -76,8 +87,8 @@ equal:
         movl $1, %eax           # eax = 1 (valeur de retour)
 
 end:
-        subl $8, %esp           # pour depiler les variables locales
-        popl %esi               # pour depiler les registres esi, edi et ebx      
+        addl $16, %esp          # pour depiler les espaces memoires allouees
+        popl %esi               # pour depiler/liberer les registres esi, edi et ebx      
         popl %edi
         popl %ebx
         leave          /* Restore ebp and esp */
