d23c8416a04d07bfaef90e2e39a42615e700982a
legil <lea-kim.gilbert@polymtl.ca>
2020-03-25 19:40:50 -0400

    transpose DONE

--- TP3/transpose.s
+++ TP3/transpose.s
@@ -5,66 +5,54 @@
 matrix_transpose_asm:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
+
+        pushl %ebx              # add working register
+        sub $8, %esp            # local variables r, c
+
+        movl $0, -8(%ebp)       # r = 0
+
+for_loop1:
+        movl -8(%ebp), %eax     # eax = r
+        cmp 16(%ebp), %eax      # compare matorder with r
+        jnb end
+        movl $0, -12(%ebp)      # c = 0
         
-        subl $8, %esp           # memory for local variables r, c
+for_loop2:
+        movl -12(%ebp), %eax    # eax = c
+        cmp 16(%ebp), %eax      # compare matorder with c
+        jnb change_row          # if !(c < matorder), change row
 
-        push %edi
+        # value of intmatdata[r + c * matorder]
+        movl 16(%ebp), %ebx     # ebx = matorder
+        mull %ebx               # eax = c * matorder
+        movl -8(%ebp), %ebx     # ebx = r
+        addl %ebx, %eax         # eax = r + c * matorder
+        movl 8(%ebp), %ebx      # ebx = inmatdata address
+        movl (%ebx, %eax, 4), %ebx      # ebx = inmatdata[r+c*matorder]
 
-        movl $0, -4(%ebp)       # r = 0
-
-for_loop_rows:
-
-        movl -4(%ebp), %eax     # eax = r
+        movl -8(%ebp), %eax     # eax = r
         movl 16(%ebp), %ecx     # ecx = matorder
-        cmp %ecx, %eax
-        jge end                 # jump to end if r >= matorder
-
-        movl $0, -8(%ebp)       # c = 0
-
-for_loop_column:
-        movl -8(%ebp), %eax     # eax = c
-        movl 16(%ebp), %ecx     # ecx = matorder
-        cmp %ecx, %eax
-        jge next_row
-
-        # inmatdata[r + c * matorder]
-        movl -8(%ebp), %eax     # eax = c
-        movl 16(%ebp), %ecx     # ecx = matorder
-        mul %ecx                # eax = c * matorder
-        movl -4(%ebp),%ecx      # ecx = r
-        addl %ecx, %eax         # eax = r + c * matorder
-        movl 8(%ebp), %ecx      # ecx = address of intmatdata
-        movl (%ecx,%eax,4), %edi  # edi = intmatdata[r + c * matorder]
-
-        movl 12(%ebp), %edx     # edx = adress of outmadata
-        
-        # movl -4(%ebp), %eax     # eax = r
-        movl 16(%ebp), %ecx     # ecx = matorder
-        mul %ecx                # eax = r * matorder
-        movl -8(%ebp),%ecx      # ecx = c
+        mull %ecx               # eax = r * matorder
+        movl -12(%ebp), %ecx    # ecx = c
         addl %ecx, %eax         # eax = c + r * matorder
+        movl 12(%ebp), %ecx     # ecx = outmatdata address
+        
+        movl %ebx, (%ecx,%eax,4)        # outmatdata[c + r * matorder] = inmatdata[r + c * matorder]
 
-        # move inmatdata into outmatdata
-        movl %edi, (%edx,%eax,4)        # outmatdata[c + r * matorder] = inmatdata[r + c * matorder]
-
-        # next row
-        movl -8(%ebp), %eax             # eax = c
-        addl $1, %eax                   # eax = c + 1
-        movl %eax, -8(%ebp)             # c = c + 1
-
-        jmp for_loop_column
-
-next_row:
-        movl -4(%ebp), %eax     # eax = r
-        addl $1, %eax           # eax = r + 1
-        movl %eax, -4(%ebp)     # r = r + 1
-        jmp for_loop_rows
-        # todo
+        movl -12(%ebp), %edx    # edx = c
+        incl %edx               # c + 1
+        movl %edx, -12(%ebp)    # c = c + 1
+        jmp for_loop2               # jumo to loop2
+        
+change_row:
+        movl -8(%ebp), %eax     # eax = r
+        incl %eax               # r + 1
+        movl %eax, -8(%ebp)     # r = r + 1
+        jmp for_loop1               # jump to loop1
 
 end:        
-        pop %edi
-
-        addl $8, %esp
+        addl $8, %esp           # remove local variables from stack
+        popl %ebx               # remove reserved register
 
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
