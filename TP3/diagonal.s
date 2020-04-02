70ad04e89ef5e1ac355ba3465a4d8c5a19b570bb
Rui Jie Li <rui-jie.li@polymtl.ca>
2020-04-02 19:00:14 -0400

    multiply and diag work

--- TP3/diagonal.s
+++ TP3/diagonal.s
@@ -3,112 +3,79 @@
 matrix_diagonal_asm:
     push %ebp      			/* Save old base pointer */
     mov %esp, %ebp 			/* Set ebp to current esp */
-    
-    mov 8(%ebp), %eax
-    mul %eax
-
-    # valeur de nb element stocker sur -4(ebp)
-    push %eax
-    
-    # esi will be row
-    mov $0, %esi
-    # ebx will be colomn
-    mov $0, %ebx
-    
-    jmp boucle
-
-boucle:
-    cmpl %esi, 8(%ebp)
-    ja fin
-    # init ebx (colomn)
+    /*esi is row*/
+    movl $0, %esi
+    /*ebx is col*/
     movl $0, %ebx
-    jmp boucle_2
 
-boucle_2:
-    # compare colomn to 8(%ebp), go to boucle if above
-    cmpl %ebx, 8(%ebp)
-    # go back to the 1st loop
-    ja incr
+    jmp for_loop1
 
-    # check if the two matrices are equal at index
-    # in: ebp - 8 - 4(cIndex + 10*rIndex)
-    # store this in ecx
-    # ecx = 10
-    movl $10, %ecx
-    # eax = esi (row)
-    movl %esi, %eax
-    # eax = 10*row
-    mul %ecx
-    # eax = 10*row + colomn
-    addl %ebx, %eax
-    # ecx = 4
-    movl $4, %ecx
-    # eax = 4(10*row + colomn)
-    mul %ecx
-    # ecx contains ebp
-    movl %ebp, %ecx
-    # ecx = ebp - 8
-    subl $8, %ecx
-    # ecx = ebp - 8 - 4(10*row + colomn)
-    subl %eax, %ecx
+for_loop1:
+    /*esi is row*/
+    cmpl %esi, 16(%ebp)
+    jb end
+    /*ebx is col*/
+    movl $0, %ebx      # c = 0
+    
+    
+    jmp for_loop2
+    
+for_loop2:
 
-    # out: ebp - 8 - 4(cIndex - 10*rIndex - nbElements
-    # edx = 10
-    movl $10, %edx
-    # eax = esi (row)
-    movl %esi, %eax
-    # eax = 10*row
-    mul %edx
-    # eax = 10*row + colomn
-    addl %ebx, %eax
-    # eax = 10*row + colomn + nbElements
-    addl -4(%ebp), %eax
-    # edx = 4
-    movl $4, %edx
-    # eax = 4(10*row + colomn + nbElements)
-    mul %edx
-    # edx contains ebp
-    movl %ebp, %edx
-    # edx = ebp - 8
-    subl $8, %edx
-    # edx = ebp - 8 - 4(10*row + colomn + nbElements)
-    subl %eax, %edx
 
-    # addresses pushed on the stack
-    push %ecx
-    push %edx
-    # values pushed on the stack
-    push (%ecx)
-    push (%edx)
+    /*edi will contain the value of which case is used*/
+    
+    /*esi is row*/
+    movl %esi, %eax /* eax = row */
+    movl 16(%ebp), %edi  /* edi = mo  */
+    mul %edi        /* eax = mo*r */
+    /*ebx is col*/
+    addl %ebx, %eax /* eax = mo*r + col*/
+    movl $4, %edi
+    mul %edi        /* eax = 4(mo*r + col)*/
+    
+
+    /*ecx is address of 1st element of inmatrix  */
+    movl 8(%ebp), %ecx
+    /*edx is address of 1st element of outmatrix */
+    movl 12(%ebp), %edx
+
+    /*both addresses are updated 1st + 4(mo*r + c)*/
+    addl %eax, %ecx
+    addl %eax, %edx
+
+    /*compare ebx (c) and esi (r)*/
+    cmpl %esi, %ebx
+    jne else
+    
 
     movl (%ecx), %ecx
-    movl (%edx), %edx
+    movl %ecx, (%edx)
 
-    cmpl %ecx, %edx
-    jne else
-
-    # addresses back 
-    movl -8(%ebp), %ecx
-    movl -12(%ebp), %edx
-
+    jmp incr_2
 
 
 else:
-    # do something
-    movl $0, 4(,%eax,4)
+    /*edx contains address of outmatrix*/
+    movl $0, %edi
+    movl %edi, (%edx)
     jmp incr_2
 
-incr:
-    # esi is row, so add 1 to row
-    addl $1, %esi     
-    jmp boucle
-
 incr_2:
-    # ebx is col, so add 1 to col
     addl $1, %ebx
-    jmp boucle_2
+    cmpl %ebx, 16(%ebp)
+    jb incr_1
+
+    jmp for_loop2
+
+incr_1:
+    addl $1, %esi
+    jmp for_loop1
 
 
-fin:
-    leave
-    ret
+end:        
+    addl $8, %esp           # remove local variables from stack
+    popl %ebx               # remove reserved register
+
+    leave          /* restore ebp and esp */
+    ret            /* return to the caller */
--- TP3/multiply.s
+++ TP3/multiply.s
@@ -1,10 +1,106 @@
-.globl matrix_multiply_asm
+.global matrix_multiply_asm
 
 matrix_multiply_asm:
-        push %ebp      /* save old base pointer */
-        mov %esp, %ebp /* set ebp to current esp */
-        
-        /* Write your solution here */
-        
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+    push %ebp      /* save old base pointer */
+    mov %esp, %ebp /* set ebp to current esp */
+    /* Write your solution here */
+
+    /* esi is r */
+    movl $0, %esi
+    /* edi is c */
+    movl $0, %edi
+    /* ebx is i */
+    movl $0, %ebx
+    jmp for_loop_1
+
+for_loop_1:
+    cmpl %esi, 20(%ebp)
+    jb end
+    movl $0, %edi
+    jmp for_loop_2 
+
+for_loop_2:
+    movl $0, %ebx
+
+    movl $0, %eax
+    pushl %eax
+    
+    jmp for_loop_3
+
+
+for_loop_3:
+    /*i is ebx*/
+    /*r is esi*/
+    /*c is edi*/
+
+    /*inmatdata1[i + r * matorder]*/
+    
+    movl %esi, %eax     /*eax = r*/
+    movl 20(%ebp), %ecx /*ecx = mo*/
+    mul %ecx            /*eax = r*mo*/
+    addl %ebx, %eax     /*eax = i + r*mo*/
+    movl $4, %ecx       /*ecx = 4*/
+    mul %ecx            /*eax = 4(i + r*mo)*/
+    movl 8(%ebp), %edx  /*edx = adress of in1*/
+    addl %eax, %edx     /*edx = add_in1 + 4(i + r*mo) */
+    push %edx    
+
+    /* inmatdata2[c + i * matorder]*/
+    movl %ebx, %eax     /*eax = i*/
+    movl 20(%ebp), %ecx /*ecx = mo*/
+    mul %ecx            /*ecx = i*mo*/
+    addl %edi, %eax     /*eax = c + i*mo*/
+    movl $4, %ecx       /*ecx = 4*/
+    mul %ecx            /*eax = 4(c + i*mo)*/
+    movl 12(%ebp), %ecx /*ecx = address of in2*/
+    addl %eax, %ecx     /*ecx = add_in2 + 4(c + i*mo) */
+
+    popl %edx           /*edx = add_in1 + 4(i + r*mo)*/
+
+    movl (%edx), %eax   /*eax = inmatdata1[i + r * matorder]*/
+    movl (%ecx), %ecx   /*ecx = inmatdata2[c + i * matorder]*/
+    mul %ecx            /*eax = inmatdata1[i + r * matorder] 
+                                * inmatdata2[c + i * matorder]*/
+    addl %eax, -4(%ebp)
+
+    jmp incr_3
+
+
+incr_3:
+    addl $1, %ebx
+    cmpl 20(%ebp), %ebx
+    jnb incr_2
+    jmp for_loop_3
+
+incr_2:
+    /*i is ebx*/
+    /*r is esi*/
+    /*c is edi*/
+    /*outmatdata[c + r * matorder] = elem;*/
+    movl %esi, %eax     /*eax = r*/
+    movl 20(%ebp), %ecx /*ecx = mo   */
+    mul %ecx            /*eax = r*mo */
+    addl %edi, %eax     /*eax = c+ r*mo*/
+    movl $4, %ecx       /*ecx = 4*/
+    mul %ecx            /*eax = 4(c+ r*mo)*/
+    movl 16(%ebp), %ecx /*ecx = start_out*/
+    addl %eax, %ecx     /*ecx = start_out + 4(c+ r*mo)*/
+    popl %edx           /*%edx = -4(%ebp)*/
+    
+    movl %edx, (%ecx)
+
+    addl $1, %edi
+
+
+    cmpl %edi, 20(%ebp)
+    jb incr_1
+    jmp for_loop_2
+
+incr_1:
+    addl $1, %esi
+    jmp for_loop_1
+    
+end:
+    leave          /* restore ebp and esp */
+    ret            /* return to the caller */
+
