aea8086878289795323a322c51370fd83cec8bba
legil <lea-kim.gilbert@polymtl.ca>
2020-04-10 16:21:48 -0400

    rectangle diagonal done

--- TP4/Ex_1/Rectangle_Diagonal.s
+++ TP4/Ex_1/Rectangle_Diagonal.s
@@ -4,7 +4,19 @@ _ZNK9Rectangle11DiagonalAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        /* Write your solution here */
+        movl 8(%ebp), %eax      # eax = this
+
+        fld 4(%eax)     # st[0] = this->length
+        fld 4(%eax)     # st[0] = this->length, st[1] = this.length
+        fmulp           # st[0] = this->length * this->length
+
+        fld 8(%eax)     # st[0] = this->width, st[1] = this->length * this->length
+        fld 8(%eax)     # st[0] = this->width, st[1] = this->width, st[2] = this->length * this->length
+        fmulp           # st[0] = this->width * this->width, st[1] = this->length * this->length
+
+        faddp           # st[0] = this->width * this->width + this->length * this->length
+
+        fsqrt
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
